b21a67c79535dca425a7ca3586b5de7bc94903c2
Acul4321 <lucarogers666@gmail.com>
2026-02-03 20:25:39 +0000

    project skeleton

---
+++ tests/main_test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "@std/assert";
+import server from "../src/main.ts";
+
+Deno.test(async function serverFetch() {
+  const req = new Request("https://deno.land");
+  const res = await server.fetch(req);
+  assertEquals(await res.text(), "Home page");
+});
+
+Deno.test(async function serverFetchNotFound() {
+  const req = new Request("https://deno.land/404");
+  const res = await server.fetch(req);
+  assertEquals(res.status, 404);
+});
+
+Deno.test(async function serverFetchUsers() {
+  const req = new Request("https://deno.land/users/123");
+  const res = await server.fetch(req);
+  assertEquals(await res.text(), "123");
+});
